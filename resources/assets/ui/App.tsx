cc2832be357690d1608d4086f898346bdb5aad9c
danikarik <baitursynov92@gmail.com>
2018-12-29 13:43:02 +0600

    change layout

--- resources/assets/ui/App.tsx
+++ resources/assets/ui/App.tsx
@@ -1,14 +1,11 @@
 import { Row, Spin } from 'antd';
-import CSRFToken from './components/CSRFToken';
 import * as React from 'react';
-import './App.css';
 
 
 class App extends React.Component {
     public render() {
         return (
             <div>
-                <CSRFToken/>
                 <Row type="flex" justify="space-around" align="middle">
                     <Spin size="large" />
                     <Spin size="large" />
--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1111:function(n,o){},29:function(n,o,p){p(30),n.exports=p(1111)},30:function(n,o){}},[[29,0,1]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1105:function(n,o){},28:function(n,o,p){p(29),n.exports=p(1105)},29:function(n,o){}},[[28,0,1]]]);
 //# sourceMappingURL=app.js.map
